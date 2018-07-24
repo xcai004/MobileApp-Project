7c1a7762c117958ec5de65d54546929c9e15cb9f
Chris Elias <Chris24elias@gmail.com>
2018-07-24 18:53:18 -0400

    login page and sign up page

--- MobileAppProject/SignupViewController.swift
+++ MobileAppProject/SignupViewController.swift
@@ -1,46 +1,38 @@
-
+//
+//  SignupViewController.swift
+//  MobileAppProject
+//
+//  Created by Chris Elias on 7/24/18.
+//  Copyright © 2018 Chris Elias. All rights reserved.
+//
 
 import Foundation
 import UIKit
 
-class LoginViewController: UIViewController {
-    @IBOutlet weak var LoginButton: UIButton!
+class SignupViewController: UIViewController {
     @IBOutlet weak var SignupButton: UIButton!
-    @IBOutlet weak var GuestButton: UIButton!
     
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var email: UITextField!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        LoginButton.backgroundColor = UIColor(red:0.00, green:0.60, blue:1.00, alpha:1.0)
-        LoginButton.setTitleColor(UIColor.white, for: .normal)
         
         SignupButton.backgroundColor = UIColor(red:0.00, green:0.60, blue:1.00, alpha:1.0)
         SignupButton.setTitleColor(UIColor.white, for: .normal)
         
-        GuestButton.backgroundColor = UIColor(red:0.00, green:0.60, blue:1.00, alpha:1.0)
-        GuestButton.setTitleColor(UIColor.white, for: .normal)
-        
-        
-    }
-    @IBAction func LoginPressed(_ sender: Any) {
-        //
         
     }
     @IBAction func SignupPressed(_ sender: Any) {
         //
         
     }
-    @IBAction func GuestPressed(_ sender: Any) {
-    }
+
     
     
-   
     
     
 }
