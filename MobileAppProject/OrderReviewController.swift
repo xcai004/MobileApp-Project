76ae4a16204a521495320156436bd722563a48ea
COP4655 <cop4655@unknown080027ece14b.attlocal.net>
2018-07-31 16:20:45 -0400

    Commented Order Review Controller

--- MobileAppProject/OrderReviewController.swift
+++ MobileAppProject/OrderReviewController.swift
@@ -11,28 +11,32 @@ import UIKit
 
 class OrderReviewController : UIViewController {
     
-    
+    //Used to display order summary before final submittion
     @IBOutlet weak var detailsTextView: UITextView!
     
+    //Will hold order summary text updated via prepare segue from
+    //checkoutView
     var details = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        detailsTextView.text = details
+        detailsTextView.text = details   //transfer received text to textView
         
-        CartModel.sharedInstance.clearCart()
+        CartModel.sharedInstance.clearCart() //remove items from cart
         
-        
+        //Set SMTP connection settings
         let smtpSession:MCOSMTPSession = MCOSMTPSession()
         smtpSession.hostname = "smtp.gmail.com"
         smtpSession.port = 465
         smtpSession.username = "mail.cotton.app@gmail.com"
+        //FUTURE-Encryp username and password
         smtpSession.password = "$%^SPWNM987@%er"
         smtpSession.authType = MCOAuthType.saslPlain
         smtpSession.connectionType = MCOConnectionType.TLS
         
+        //Create E-mail Message
         let builder:MCOMessageBuilder = MCOMessageBuilder()
         let from:MCOAddress = MCOAddress(displayName: "abc", mailbox: "mail.cotton.app@gmail.com")
         let to:MCOAddress = MCOAddress(displayName: nil, mailbox: "xcai004@fiu.edu")
@@ -42,6 +46,8 @@ class OrderReviewController : UIViewController {
         builder.htmlBody = details
         let rcf822Data = builder.data()
         let sendOperation:MCOSMTPSendOperation = smtpSession.sendOperation(with: rcf822Data)
+        
+        //Send the Email
         sendOperation.start { (error) -> Void in
             if (error != nil) {
                 print("Error Sending Mail : \(error)")
